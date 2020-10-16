d23ce357f4ae5f35cc24bb51d5355eb29cce634f
Jonathan V. Solórzano <JonathanVSV@users.noreply.github.com>
2020-10-16 14:04:55 -0500

    First commit

---
+++ Data_preprocess.R
@@ -0,0 +1,285 @@
+library(raster)
+library(rray)
+library(Hmisc)
+library(imager)
+library(reticulate)
+
+# Number of squared samples (windows of x by x pixels) for which you have training data
+num_imagenes <- 23
+
+# Use numbers to name training data of image samples (reflectance and backscatter values) 
+# and image masks (land use land cover [LULC] categories)
+nombres_mask <- seq(1,23,1)
+
+# Number of LULC classes
+n_classes <- 11
+# Size of windows in pixels (training data)
+img_width <- 256
+img_height <- 256
+# Size of windows in pixels (augumented training data)
+img_width_exp <- 128
+img_height_exp <- 128
+# Number of bands of the input image
+channels <- 6
+
+# Number of 128pix-squares that are going to be obtained per 256 pix-squares
+# In this case we are going to use 25 (5 x 5 128 x 128 pix) using a 32 pix offset
+# This is cropping the image as 1-129, 1-129; 33-161, 1-129; 65-193, 1-129; 97-225, 1-129; 129-256, 1-129 
+num_squares <- 5^2
+probs_4crops <- 1 / (sqrt(num_squares)-1) 
+# Number of mirrored images per 128pix-image
+num_mirrors <- 2
+
+# ---------------Read Training Info----------------------------------------------------
+# Read files for rectangles for which you have training data (land cover / land use)
+cuads <- list.files("D:/Drive/Jonathan_trabaggio/Doctorado/GeoInfo/TiposVeg_points/cuads",
+                    "*.shp$",
+                    full.names = T)
+cuads <- lapply(cuads, shapefile)
+
+# Locations of image files
+optic_names <- "D:/Drive/Jonathan_trabaggio/Doctorado/GeoInfo/Sentinel-2/Cropped_GEE/MCBA/4B_10m_2019-7-4.tif"
+radar_names <- "D:/Drive/Jonathan_trabaggio/Doctorado/GeoInfo/Sentinel-1y-2_comb/SingleDate_2019-06-15-07-15_VV_VH_RGI_gamma0_MaxCCL20.tif"
+
+# Read images as stacks
+optic <- stack(optic_names)
+radar <- stack(radar_names)
+
+# Input data that is going to be pre processed to further feed the U-net
+# Features: Stack optic 4 bands and 2 radar bands
+optic <- stack(optic,radar[[1:2]])
+
+# Labels: Read files of manually classificated areas 
+masks_list <- list.files("D:/Drive/Jonathan_trabaggio/Doctorado/GeoInfo/TiposVeg_points/masks",
+                         "*.shp$",
+                         full.names = T)
+masks <- lapply(masks_list, shapefile)
+
+# ---------------Crop Images----------------------------------------------------
+# Crop images (features data) to extents of shapefiles
+cropped_im <- lapply(cuads, function(x){
+  temp <- crop(optic, x)
+})
+
+# Crop class info (labels data) to extents of shapefiles
+cropped_masks <- lapply(1:length(masks), function(i){
+  temp <- crop(masks[[i]], cuads[[i]])
+  temp@data$id = as.integer(temp@data$id)
+  rasterize(temp, cropped_im[[i]],
+            field = "id",
+            background = 0)
+  
+})
+
+# Create an empty list to save cropped class info
+y_rast_list <- vector(mode = "list", 
+                      length = length(cropped_masks))
+
+# Change masks to binary representations
+# List with two dimensions, first index is number of image
+# Second index is type of cover mask
+for (j in 1:length(cropped_masks)){
+  y_rast_list[[j]] <- stack(lapply(1:n_classes, function(i){
+    cropped_masks[[j]] == i
+  }))
+}
+
+# Plot
+plot(cropped_im[[7]])
+plot(y_rast_list[[7]])
+
+# ---------------Convert info to arrays----------------------------------------------------
+# Change image (features data) to type array
+cropped_im_matr <- lapply(cropped_im, as.array)
+# Change class info (labels data) to type array
+cropped_mask_matr <- lapply(y_rast_list, as.array)
+
+# Create empty arrays to fill it with the previous info
+train_x_data<-array(0,
+                    dim = c(length(cropped_im_matr),
+                            img_height,
+                            img_width,
+                            channels))
+train_y_data<-array(0,
+                    dim = c(length(cropped_im_matr),
+                            img_height,
+                            img_width,
+                            n_classes))
+
+# Fill the arrays
+for(i in 1:length(cropped_im_matr)) {
+  train_x_data[i,1:img_width,1:img_height,1:channels] <- cropped_im_matr[[i]]
+}
+
+for(i in 1:length(cropped_mask_matr)) {
+    train_y_data[i,1:img_width,1:img_height,1:n_classes] <- cropped_mask_matr[[i]]
+}
+
+# ---------------Create 128x128 px arrays----------------------------------------------------
+# Create 128 x 128 arrays
+train_x_data_128<-array(0,
+                    dim = c(length(cropped_im_matr) * num_squares,
+                            img_height_exp,
+                            img_width_exp,
+                            channels))
+train_y_data_128<-array(0,
+                        dim = c(length(cropped_mask_matr) * num_squares,
+                                img_height_exp,
+                                img_width_exp,
+                                n_classes))
+
+# Vectors with starting positions to make 128 x 128 pix squares
+temp <- unique(quantile(seq(1,img_width_exp+1), probs = seq(0, 1, probs_4crops)))
+# The valuse that are going to be used to crop
+crops <- c(temp,img_width_exp+1)
+crops
+
+# Value to sum to each of the previous values
+sum_pix <- img_height_exp - 1
+aux <- 1
+
+# Fill the empty arrays with the info, 256 x 256 images get cropped in the 128 x 128 px
+for(i in 1:dim(train_x_data)[1]) {
+  for(j in 1:length(crops)){
+    for(k in 1:length(crops)){
+      train_x_data_128[aux,,,] <- train_x_data[i,crops[j]:(crops[j]+sum_pix),
+                                                 crops[k]:(crops[k]+sum_pix),
+                                                 1:channels]
+      aux <- aux + 1
+    }
+  }
+}
+
+aux <- 1
+for(i in 1:dim(train_y_data)[1]) {
+  for(j in 1:length(crops)){
+    for(k in 1:length(crops)){
+      train_y_data_128[aux,,,] <- train_y_data[i,crops[j]:(crops[j]+sum_pix),
+                                               crops[k]:(crops[k]+sum_pix),
+                                               1:n_classes]
+      aux <- aux + 1
+    }
+  }
+}
+
+# ---------------Mirrored Images----------------------------------------------------
+# Get number of total images (original + 2 mirrors)
+multip_im <- num_mirrors + 1
+
+# Create 128 x 128 empty arrays, self + 2 mirrors
+train_x_data_final<-array(0,
+                        dim = c(dim(train_x_data_128)[1] * multip_im,
+                                img_height_exp,
+                                img_width_exp,
+                                channels))
+train_y_data_final<-array(0,
+                        dim = c(dim(train_y_data_128)[1] * multip_im,
+                                img_height_exp,
+                                img_width_exp,
+                                n_classes))
+
+
+aux <- 1
+
+# Create mirrored images and fill the empty arrays
+for(i in 1:dim(train_x_data_128)[1]) {
+  train_x_data_final[aux,,,] <- train_x_data_128[i,,,]
+  aux = aux + 1
+  train_x_data_final[aux,,,] <- rray_flip(train_x_data_128[i,,,],axis = 1)
+  aux = aux + 1
+  train_x_data_final[aux,,,] <- rray_flip(train_x_data_128[i,,,],axis = 2)
+  aux = aux + 1
+}
+
+aux <- 1
+
+for(i in 1:dim(train_y_data_128)[1]) {
+  train_y_data_final[aux,,,] <- train_y_data_128[i,,,]
+  aux = aux + 1
+  train_y_data_final[aux,,,] <- rray_flip(train_y_data_128[i,,,],axis = 1)
+  aux = aux + 1
+  train_y_data_final[aux,,,] <- rray_flip(train_y_data_128[i,,,],axis = 2)
+  aux = aux + 1
+}
+
+# Check if everything is ok
+par(mfrow = c(2,2))
+plot(train_x_data_final[207,1:128,1:128,1:3] %>% as.cimg())
+plot(train_y_data_final[207,1:128,1:128,6] %>% as.cimg())
+plot(train_y_data_final[207,1:128,1:128,11] %>% as.cimg())
+
+# ---------------Normalize Images----------------------------------------------------
+# Before exporting all the data, let's calculate per band mean and SD to normalize the data
+mean_x <- apply(train_x_data_final, 4, mean)
+sd_x <- apply(train_x_data_final, 4, sd)
+
+# Normalize data (only features data)
+for(i in 1:dim(train_x_data_final)[1]){
+  for(j in 1:dim(train_x_data_final)[4]){
+    train_x_data_final[i,,,j] <- (train_x_data_final[i,,,j] - mean_x[j]) / sd_x[j]
+  }
+}
+
+#---------------Image checks----------------------------------------------------
+#Check min and max values
+apply(train_x_data_final, 4, min)
+apply(train_x_data_final, 4, max)
+
+# Check NA, NaN, Inf, -Inf; which might cause the loss value to fix in Nan, when training the U-net 
+which(apply(train_x_data_final, 1, is.nan))
+which(apply(train_x_data_final, 1, is.na))
+which(apply(train_x_data_final, 1, function(x) x == Inf))
+which(apply(train_x_data_final, 1, function(x) x == -Inf))
+
+which(apply(train_y_data_final, 1, is.nan))
+which(apply(train_y_data_final, 1, is.na))
+which(apply(train_y_data_final, 1, function(x) x == Inf))
+which(apply(train_y_data_final, 1, function(x) x == -Inf))
+
+# Check normalized images
+plot(train_x_data_final[500,1:128,1:128,3:1] %>% as.cimg())
+plot(train_y_data_final[500,1:128,1:128,6] %>% as.cimg())
+plot(train_y_data_final[500,1:128,1:128,4] %>% as.cimg())
+
+# ---------------Test and Train sets----------------------------------------------------
+# Create train and test sets
+# Number of images (128 x 128 px) that come from the same 256 x 256 image
+imgs_share <- (multip_im * num_squares) - 1
+
+# Images that are going to be saved as test data; and thus, removed from the training data
+# With this procedure we completely omit all the augumented images from a single
+# 256 x 256 px square.
+testers <- c(1+imgs_share,imgs_share*10+1)
+
+# Define test and training data
+test_x_data_final <- train_x_data_final[c(1:testers[1],testers[2]:(testers[2]+imgs_share)),,,]
+test_y_data_final <- train_y_data_final[c(1:testers[1],testers[2]:(testers[2]+imgs_share)),,,]
+
+train_x_data_final <- train_x_data_final[-c(1:testers[1],testers[2]:(testers[2]+imgs_share)),,,]
+train_y_data_final <- train_y_data_final[-c(1:testers[1],testers[2]:(testers[2]+imgs_share)),,,]
+
+# Plot images to check that everything is ok
+plot(train_x_data_final[1,1:img_height_exp,1:img_width_exp,3:1] %>% as.cimg())
+plot(train_y_data_final[1,1:img_height_exp,1:img_width_exp,8] %>% as.cimg())
+plot(train_y_data_final[1,1:img_height_exp,1:img_width_exp,11] %>% as.cimg())
+
+# One option is to save files as rdf
+# Save as rdf files
+# saveRDS(train_x_data_final, file = "train_x_data.rds", ascii = F)
+# saveRDS(train_y_data_final, file = "train_y_data.rds", ascii = F)
+# 
+# saveRDS(test_x_data_final, file = "test_x_data.rds", ascii = F)
+# saveRDS(test_y_data_final, file = "test_y_data.rds", ascii = F)
+
+# ---------------Export to npz----------------------------------------------------
+# Other option: saving files as numpy zip files
+# This option was prefered as a single file can contain both training and test data
+np <- import("numpy")
+
+np$savez("Lacandona_LULC_2019_OpticRadar_Augment16perim.npz", 
+         x_train = train_x_data_final, 
+         y_train = train_y_data_final, 
+         x_test = test_x_data_final, 
+         y_test = test_y_data_final)
+
+
---
+++ Result_evaluation.R
@@ -0,0 +1,328 @@
+library(keras)
+library(tidyverse)
+library(raster)
+library(rgdal)
+library(reticulate)
+library(rasterVis)
+
+# Hyperparameters for training U-net
+batch_size <- 64
+# Channels in features input data
+channels <- 6
+learn_rate <- 1e-4
+epochs <- 50
+dropout <- 0.5
+# Number of filters in first layer of U-net
+# Consecutive layers will duplicate the number of filters of the previous one
+filters_firstlayer <- 64
+# Number of layers for the U-net
+num_layers <- 3
+# Output activation function
+activation_func_out <- "softmax"
+
+#Label data; number of classes in labeled data
+n_classes <- 11 
+
+# Images dimensions in px
+img_width <- 128
+img_height <- 128
+
+# Image dimensions in px for predicted ones
+img_width_pred <- 128
+img_height_pred <- 128
+
+##---------------------Image Folders Definition-------------------------
+#Training
+#Image folder
+imagery <- "OpticRadar"
+
+#Folder containing the npz file
+train_image_files_path <- file.path(paste0("D:/Drive/Jonathan_trabaggio/Doctorado/R/PhD_DeepLearning/Img_Preprocess"))
+#---------------------------Read inputs-------------------------
+
+# RDS option
+# train_x_data <- readRDS(paste0(train_image_files_path,"/","train_x_data_OptRad.rds"))
+# train_y_data <- readRDS(paste0(train_image_files_path,"/","train_y_data_OptRad.rds"))
+# 
+# # Aquí hay que hacer algo con los datos para que estén en un rango entre 0 y 1, quizás como un hist stretch
+# test_x_data <- readRDS(paste0(train_image_files_path,"/","test_x_data_OptRad.rds"))
+# test_y_data <- readRDS(paste0(train_image_files_path,"/","test_y_data_OptRad.rds"))
+
+# Numpy zip file option
+np <- import("numpy")
+
+# Load npz
+npz2 <- np$load(paste0(train_image_files_path,"/","Lacandona_LULC_2019_OpticRadar.npz"))
+# See files
+# npz2$files
+
+# Load training and test data (both x: features and y: labels)
+train_x_data <- npz2$f[["x_train"]]
+train_y_data <- npz2$f[["y_train"]]
+
+test_x_data <- npz2$f[["x_test"]]
+test_y_data <- npz2$f[["y_test"]]
+
+# Check some images
+par(mfrow = c(2,2))
+plot(train_x_data[1,,,1:3] %>% imager::as.cimg())
+plot(train_x_data[1,,,4:6] %>% imager::as.cimg())
+plot(train_y_data[1,,,11] %>% imager::as.cimg())
+plot(train_y_data[1,,,8] %>% imager::as.cimg())
+
+# ------------------------------Load previously saved model-----------------------------
+# Define dice to be able to load model
+dice <- custom_metric("dice", function(y_true, y_pred, smooth = 1) {
+  y_true_f <- k_flatten(y_true)
+  y_pred_f <- k_flatten(y_pred)
+  intersection <- k_sum(y_true_f * y_pred_f)
+  (2 * intersection + smooth) / (k_sum(y_true_f) + k_sum(y_pred_f) + smooth)
+})
+
+
+model <- load_model_hdf5(paste0("modelEpochs1000_lr7e-04_adam_S2_2019-07-04",".h5"),
+                        custom_objects = c("dice" = dice))
+
+
+#----------------------Predict images from test set-------------------------------------
+
+# Create an empty vector to calculate the overall accuracy for each predicted image
+accuracy <- vector(length = dim(test_x_data)[1])
+# Create an empty list to save the confusion matrix for each predicted image
+conf_matrix <- vector(length = dim(test_x_data)[1], mode = "list")
+
+# Names of the classes that we are using
+classes <- c("NoData","PlantacionesMaduras","BosqueMaduro","BosqueSecundario","Caminos",
+             "PlantacionesJovenes","Suelo","Cultivos/Pastizales","Agua","Nubes","Sombras",
+             "AsentamientoHumano")
+
+# Create two color pallettes 1) to plot classified images and
+# 2) plot probability of each pixel of belonging to the predicted class
+pal <- colorRampPalette(c("red","green"))
+pal2 <- colorRampPalette(c("red","orange"))
+
+# Para exportar los plots nada más modificar el ciclo y poner anter y después del ciclo el jpeg y dev.off
+aux <- 1.1
+
+every3 <- seq(1, dim(test_x_data)[1], 3)
+end3 <- seq(3, dim(test_x_data)[1], 3)
+
+# Predict each test image and calculate its confusion matrix
+for(j in 1:dim(test_x_data)[1]){
+  
+  # Get features data (images)
+  image_real<-array_reshape(test_x_data[j,,,1:channels],c(1,img_width_pred,img_height_pred,channels))
+  
+  # Predict class (y_pred)
+  result_pred <- predict(
+    object = model,
+    batch_size = batch_size,
+    x = image_real,
+    steps = 10,
+    verbose = 1)
+   
+  # Get label data (classess)
+  image_class <- array_reshape(test_y_data[j,,,1:n_classes],c(1,img_width_pred,img_height_pred,n_classes))
+  
+  # Get predicted image
+  image_pred <- array_reshape(result_pred[1,,,1:n_classes],c(1,img_width_pred,img_height_pred,n_classes))
+  
+  rast_list[[j]] <- image_pred
+  
+  # Get max probability value by cell in all the classes, i.e., dim 3
+  maxval_bycell <- apply(image_pred[1,,,] , c(1,2) , 
+                         function(x) 
+                           ifelse(all(is.na(x)), NA, max(x, na.rm = TRUE))) 
+  
+  # Set class as the pixel that match the max prob (remember this comes from softmax classificator 0-1)
+  # This is y_pred
+  resul2 <- lapply(1:n_classes, function(i){
+    temp <- image_pred[1,,,i] == maxval_bycell
+    temp[temp == TRUE] <- i
+    temp[temp == FALSE] <- 0
+    as.matrix(temp)
+    
+  })
+  
+  # Reduce resuls to transform binary bands to multiband image
+  resul2 <- Reduce('+', resul2)
+  
+  # Set class as the pixel that match 1
+  # This is y
+  resul <- lapply(1:n_classes, function(i){
+    temp <- image_class[1,,,i]==1
+    temp[temp == TRUE] <- i
+    temp[temp == FALSE] <- 0
+    as.matrix(temp)
+    
+  })
+  # Reduce resul to transform binary bands to multiband image
+  resul_class <- Reduce('+', resul)
+  
+  # Confusion matrix, we need to complete missing levels so that the diagonal corresponds to 
+  # correctly predicted pixels
+  confusion_matrix<- table(factor(resul2,seq(0,11,1)), factor(resul_class, seq(0,11,1)))
+  conf_matrix[[j]] <- confusion_matrix
+  
+  #Calculate overall precision
+  accuracy[j]<-sum(diag(confusion_matrix)) / sum(confusion_matrix)
+  
+  temp <- stack(raster(as.matrix(image_real[1,,,3])),
+                raster(as.matrix(image_real[1,,,2])),
+                raster(as.matrix(image_real[1,,,1])))
+  
+  # Export a jpeg of every 3 samples
+  if(j %in% every3) {
+    jpeg(paste0("Predicted_images", j, ".jpeg"),
+         width = 30,
+         height = 21,
+         units = "cm",
+         quality = 100,
+         res = 300)
+    par(mfrow = c(3, 5),
+        mai = c(0.1,0.1,0.1,0.1),
+        pin = c(3,3),
+        bty = 'n')
+  }
+  
+  # Plot test data image (x_test)
+  plotRGB(temp, r = 1, g = 2, b = 3,scale=800, stretch = "lin")
+  
+  # Plot test class info (y_test)
+  plot(raster(resul_class/12),
+       col=rainbow(11),
+       breaks = seq(0,1,0.1),
+       xaxt = "n",
+       yaxt = "n",
+       axes=FALSE,
+       legend = F)
+  
+  # Plot test class perdicted (y_pred_test)
+  plot(raster(resul2/12),
+       col=rainbow(11),
+       breaks = seq(0,1,0.1),
+       xaxt = "n",
+       yaxt = "n",
+       axes=FALSE,
+       legend = F)
+  
+  # Plot probability of each pixel of being of the assigned class
+  plot(raster(maxval_bycell),
+       col = pal(10),
+       breaks = seq(0,1,0.1),
+       xaxt = "n",
+       yaxt = "n",
+       axes=FALSE,
+       legend = T)
+  # Plot probability of each pixel of being of the assigned class >= 0.5
+  plot(raster(maxval_bycell >= 0.5),
+       col = pal2(6),
+       breaks = seq(0,0.5,0.1),
+       xaxt = "n",
+       yaxt = "n",
+       axes=FALSE,
+       legend = T)
+
+  if(j %in% end3) {
+    dev.off()
+  }
+  # Pplot to addlegend
+  # plot(raster(resul2/12),
+  #      col=rainbow(11),
+  #      breaks = seq(0,1,0.1),
+  #      xaxt = "n",
+  #      yaxt = "n",
+  #      axes=FALSE,
+  #      legend = F)
+  # legend(x=-1,y=aux,
+  #        legend = classes[-1],
+  #        fill=rainbow(12)[1:12],
+  #        cex = 0.9,
+  #        box.col = "transparent")
+  aux <- aux + 1.1
+  
+  aux <- ifelse(aux >= 6, 1.1, aux)
+  
+  
+}
+
+# Total mean accuracy
+mean(accuracy)
+
+# Get sum of all confusion matrices
+total_conf_mat <- Reduce('+', conf_matrix)
+colnames(total_conf_mat) <- classes
+rownames(total_conf_mat) <- classes
+total_conf_mat
+
+# Write the complete confusion matrix and accuracy
+write.csv(total_conf_mat, paste0("ConfusionMatrix_TestData_","U128model",imagery,"Epochs",epochs,"_lr",learn_rate,"_adam","_2019-10-06",".csv"))
+write.csv(mean(accuracy), paste0("MeanAccuracy_TestData_","U128model",imagery,"Epochs",epochs,"_lr",learn_rate,"_adam","_2019-10-06",".csv"))
+
+
+# Get product and user accuracy
+users_accuracy <- sapply(1:nrow(total_conf_mat), function(i){
+  total_conf_mat[i,i] / sum( total_conf_mat[i,])
+})
+names(users_accuracy) <- colnames(total_conf_mat)
+users_accuracy
+
+producers_accuracy <- sapply(1:nrow(total_conf_mat), function(i){
+  total_conf_mat[i,i] / sum( total_conf_mat[,i])
+})
+names(producers_accuracy) <- row.names(total_conf_mat)
+producers_accuracy
+
+####---------------------Visualize intermediate activation layers------------------------------------------
+model
+# Extracts the outputs of the top 2 layers:
+# Cargar ciertas capas de activación del vgg16
+block2conv <- keras_model(inputs = model$input,
+                          outputs = get_layer(model, 'conv2d_7')$output)
+
+# Returns a list of five arrays: one array per layer activation
+# Esto no sirve si utilizas la bateria
+activations <- block2conv %>% predict(test_x_data[1:2,,,])
+par(mfrow = c(10, 4), mai = c(0.1,0.1,0.1,0.1), bty = 'n')
+
+for(i in 1:39)
+{
+  temp <- activations[1,1:16,1:16, i]
+  plot(as.raster(temp/max(temp)))
+}
+
+
+dim(first_layer_activation)
+
+plot_channel <- function(channel) {
+  rotate <- function(x) t(apply(x, 2, rev))
+  image(rotate(channel), axes = FALSE, asp = 1, 
+        col = terrain.colors(12))
+}
+
+pdf("First_layer_activation.pdf",
+    width=15,
+    height = 12)
+par(mfrow = c(8, 6),mai = rep_len(0.02, 4))
+for(i in 1:48){
+  plot_channel(first_layer_activation[1,,,i])
+}
+dev.off()
+
+pdf("Second_layer_activation.pdf",
+    width=15,
+    height = 12)
+par(mfrow = c(6, 6),mai = rep_len(0.02, 4))
+for(i in 1:32){
+  plot_channel(second_layer_activation[1,,,i])
+}
+dev.off()
+
+pdf("Third_layer_activation.pdf",
+    width=15,
+    height = 12)
+par(mfrow = c(1, 1))
+for(i in 1:1){
+  plot_channel(third_layer_activation[1,,,i])
+}
+dev.off()
